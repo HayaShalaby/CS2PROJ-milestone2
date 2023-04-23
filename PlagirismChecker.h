004a3b3131adeb37f336270c02c37c8d2def7c74
HayaShalaby <124674435+HayaShalaby@users.noreply.github.com>
2023-04-23 15:48:20 +0200

    Added comments

--- PlagirismChecker.h
+++ PlagirismChecker.h
@@ -1,45 +1,32 @@
-/**
- * Project Untitled
- */
-
-
+//Haya Shalaby, Rana Taher, Jana Elmidany
 #ifndef _PLAGIRISMCHECKER_H
 #define _PLAGIRISMCHECKER_H
 
+#include <string>
+#include "Pattern.h"
+#include "Document.h"
+using namespace std;
+
 class PlagirismChecker {
-public: 
-    
-/**
- * @param userFile
- * @param ogFile
- */
-PlagirismChecker PlagirismChecker(string userFile, string ogFile);
-    
-bool isQuote();
-    
-bool isRef();
-    
-void calcPlag();
-    
-void compDoc();
-    
-int getTotalPlag();
-    
-int getTotalRef();
-    
-int getTotalSim();
-    
-int getTotalQuotes();
-    
-Document getHighlightDoc();
-private: 
-    int totalPlag;
-    int totalRef;
-    int totalSim;
-    int totalQuotes;
-    Document userDoc;
-    vector<Document> database;
-    Document highlightDoc;
+    private: 
+        int totalPlag;
+        int totalRef;
+        int totalSim;
+        int totalQuotes;
+        Document userDoc;
+        vector<Document> database;
+        Document highlightDoc;
+    public: 
+        PlagirismChecker(string userFile, string ogFile); //opens userFile and ogFile 
+        bool isQuote(); //checks if similar line is a quotation & increments totalQuotes if true 
+        bool isRef();  //looks for reference page & adds count of all words starting there to totalRef
+        void calcPlag(); //calculates totalPlag by subtracting totalRef and totalQuotes from totalSim
+        void compDoc(); //compares userDoc against database for similarity, produces highlighted document 
+        int getTotalPlag(); //getter for totalPlag
+        int getTotalRef(); //getter for totalRef 
+        int getTotalSim(); //getter for totalSim 
+        int getTotalQuotes(); //getter for totalQuotes 
+        Document getHighlightDoc(); //getter for highlightDoc  
 };
 
 #endif //_PLAGIRISMCHECKER_H
