d0330d4f9c319414ebae426f8c7479324605ac72
HayaShalaby <124674435+HayaShalaby@users.noreply.github.com>
2023-04-24 13:55:22 +0200

    Update Output.h

--- Output.h
+++ Output.h
@@ -2,21 +2,21 @@
 #ifndef _OUTPUT_H
 #define _OUTPUT_H
 
-#include "PlagirismChecker.h"
+#include "PlagiarismChecker.h"
 #include "Document.h"
 #include <string>
 #include <fstream>
 #include <iostream>
 using namespace std;
 
-class Output: public PlagirismChecker {
+class Output: public PlagiarismChecker {
     private: 
         float percentPlag;
         float percentRef;
         float percentSim;
         float percentQuotes;
     public: 
-        Output(string userFile, string ogFile): PlagirismChecker(userFile, ogFile); //constructor, executes all PlagirismChecker functions 
+        Output(string userFile, string ogFile): PlagiarismChecker(userFile, ogFile); //constructor, executes all PlagiarismChecker functions 
         void Terminal(); //displays percentages in terminal
         void file(); //displays percentages and highlighted text in file
 };
