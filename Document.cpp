851258a84ddf67971305320a738f58043aa55f4a
HayaShalaby <124674435+HayaShalaby@users.noreply.github.com>
2023-05-04 14:26:54 +0300

    Update Document.cpp

--- Document.cpp
+++ Document.cpp
@@ -1,40 +1,24 @@
-/**
- * Project Untitled
- */
+
 
 
 #include "Document.h"
+using namespace std;
 
-/**
- * Document implementation
- */
-
-
-/**
- * @param FN
- * @return Document
- */
-Document Document::Document(string FN) {
+Document::Document(string FN) {
     return null;
 }
 
-/**
- * @return string
- */
+
 string Document::getFileName() {
     return "";
 }
 
-/**
- * @return vector<string>
- */
+
 vector<string> Document::getLines() {
     return null;
 }
 
-/**
- * @return int
- */
+
 int Document::numOfWords() {
     return 0;
 }
